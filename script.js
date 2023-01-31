6e7e7f2e42a377ed1f69010090053e16178ca8f6
jasperket <jasperkeith1.vallecera@gmail.com>
2023-01-31 17:53:27 +0800

    Added function to play one round

--- script.js
+++ script.js
@@ -8,3 +8,39 @@ function getComputerChoice() {
     }
     return 'Scissors;'
 }
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toUpperCase();
+    if(playerSelection == 'ROCK') {
+        if(computerSelection == 'Rock') {
+            return 'Draw! Rock draws Rock';
+        }
+        if(computerSelection == 'Paper') {
+            return 'You lose! Paper beats Rock';
+        } 
+        return 'You win! Rock beats Scissors';
+    }
+    if(playerSelection == 'Paper') {
+        if(computerSelection == 'Rock') {
+            return 'You win! Paper beats Rock';
+        }
+        if(computerSelection == 'Paper') {
+            return 'Draw! Paper draws Paper';
+        } 
+        return 'You lose! Scissors beats Paper';
+    }
+    if(playerSelection == 'Scissors') {
+        if(computerSelection == 'Rock') {
+            return 'You lose! Rock beats Scissors';
+        }
+        if(computerSelection == 'Paper') {
+            return 'You win! Scissors beats Paper';
+        } 
+        return 'Draw! Scissors draws Scissors';
+    }
+}
+
+const playerSelection = prompt("Select your weapon! (Rock, Paper, or Scissors)");
+const computerSelection = getComputerChoice();
+
+console.log(playRound(playerSelection,computerSelection));
